5f67a7588ab06540c2fa2cc3fbe4cbacf848d944
disperate <julian.bigler@hotmail.com>
2017-03-24 09:22:09 +0100

    added travis support

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ setuptools.setup(
     author_email="julian.bigler@stud.hslu.ch",
 
     description="Software for autonomous vehicle build during PREN",
-    long_description=open('README.rst').read(),
 
     packages=setuptools.find_packages(),
 
@@ -17,11 +16,6 @@ setuptools.setup(
 
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
     ],
 )
