468afc02f8a2143d2f35f161b13e0239704b1ad9
AdrianKauz <akauz@hispeed.ch>
2017-04-04 14:56:49 +0200

    updated i2cHandler
    still in development....

--- modul/i2cHandler.py
+++ modul/i2cHandler.py
@@ -13,18 +13,25 @@
 #                 distance-values and stores it
 #               - Access to the sense-hat
 #
-# Sensors:      - Read:     On measuring
+# Sensors:      - Read:     On measuring (continuous reading)
 #                 Write:    On initialisation
-# Sense-Hat:    - Read:     Gyro-States
+# Sense-Hat:    - Read:     Gyro-States (continuous reading)
 #                 Write:    Display
+#
+# ToDo:         - PyCharm highlights rows with "ToDo" which is nice
+# ToDo:         - Testing the whole sheit
 #------------------------------------------------------------------------------
 
 # Imports
+import threading
+from time import sleep
 from modul.i2cModules import senseHatAdapter
 from modul.i2cModules import distanceSensorAdapter
 
 # Variables
 SENSOR_DATA_BUFFER_SIZE = 5
+DISPLAY_MAX_STATES      = 8
+THREAD_SLEEP_MS         = 100
 
 
 # Class
@@ -34,49 +41,115 @@ class I2cHandler(threading.Thread):
     def __init__(self):
         # Threading stuff
         threading.Thread.__init__(self)
-        self.condition = threading.Condition()
+        self.lock = threading.RLock()
 
-        # Protected field by condition
+        # Protected field by locks
         self.currYaw = [0.0] * SENSOR_DATA_BUFFER_SIZE
         self.currPitch = [0.0] * SENSOR_DATA_BUFFER_SIZE
         self.currRoll = [0.0] * SENSOR_DATA_BUFFER_SIZE
-        self.measureOrientation = False
-        self.measureDistance = False
+        self.dispStatesList = [0] * DISPLAY_MAX_STATES
+        self.dispRomanNumber = 0
+        self.threadMeasureOrientation = True
+        self.threadMeasureDistance = True
+        self.threadRefreshDisplay = False
 
         # Unprotected field
         self.threadIsRunning = True
+
+        # Used modules
+        self.senseHat = senseHatAdapter.SenseHatAdapter()
+        self.distanceSensors = distanceSensorAdapter.DistanceSensorAdapter()
 
 
     # Funktions
     # --------------------------------------------------------------------------
     def getCurrYaw(self):
         """
-        Description: Gets the yaw-value from the last forced measurement
+        Description: Gets the yaw-value from the last measurement
         Returns:     Float
         """
-        with self.condition:
-            self.condition.wait()
-            return self.currYaw[0]
+        currYaw = 0.0
+        self.lock.acquire()
+        try:
+            currYaw = self.currYaw[0]
+        except:
+            pass
+        finally:
+            self.lock.release()
+
+        return currYaw
 
 
     def getCurrPitch(self):
         """
-        Description: Gets the pitch-value from the last forced measurement
+        Description: Gets the pitch-value from the last measurement
         Returns:     Float
         """
-        with self.condition:
-            self.condition.wait()
-            return self.currPitch[0]
+        currPitch = 0.0
+        self.lock.acquire()
+        try:
+            currPitch = self.currPitch[0]
+        except:
+            pass
+        finally:
+            self.lock.release()
+
+        return currPitch
 
 
     def getCurrRoll(self):
         """
-        Description: Gets the roll-value from the last forced measurement
+        Description: Gets the roll-value from the last measurement
         Returns:     Float
         """
-        with self.condition:
-            self.condition.wait()
-            return self.currRoll[0]
+        currRoll = 0.0
+        self.lock.acquire()
+        try:
+            currRoll = self.currRoll[0]
+        except:
+            pass
+        finally:
+            self.lock.release()
+
+        return currRoll
+
+
+    def setStatus(self, newNumber, newState):
+        """
+        Description: Sets the status LED on the sense-hat for a visual feedback
+                     of a given module.
+                     Possible values (0-2): "IDLE", "RUNNING", "STOPPED"
+        Args:        1. Position as number or a string
+                     2. State value (0-2)
+        """
+        if ((0 <= newNumber) and (newNumber < DISPLAY_MAX_STATES)):
+            self.lock.acquire()
+            try:
+                self.dispStatesList[newNumber] = newState
+            except:
+                pass
+            finally:
+                self.threadRefreshDisplay = True
+                self.lock.release()
+
+        return
+
+
+    def setRomanNumber(self, newNumber):
+        """
+        Description: Sets the roman number
+        Args:        1. Number as integer or string
+        """
+        self.lock.acquire()
+        try:
+            self.dispRomanNumber = int(newNumber)
+        except:
+            pass
+        finally:
+            self.threadRefreshDisplay = True
+            self.lock.release()
+
+        return
 
 
     def run(self):
@@ -84,28 +157,34 @@ class I2cHandler(threading.Thread):
         Running thread, which will do all the work for this module.
         """
         while (self.threadIsRunning):
-            with self.condition:
+            self.lock.acquire()
+            try:
                 # Write stuff to display
+                if(self.threadRefreshDisplay):
+                    self.senseHat.setStatusLeds(self.dispStatesList)
+                    self.senseHat.setRomanNumber(self.dispRomanNumber)
+                    self.senseHat.refreshDisplay()
 
-                if(self.measureDistance):
+                    self.threadRefreshDisplay = False
+
+                if (self.threadMeasureDistance):
                     # Do measurements and save the results locally
                     pass
 
-                if(self.measureOrientation):
-                    newYaw = 0.0
-                    newRoll = 0.0
-                    newPitch = 0.0
+                if (self.threadMeasureOrientation):
+                    if(self.senseHat.refreshOrientation()):
+                        # get orientation
+                        self.currYaw.append(self.senseHat.getYaw())
+                        self.currPitch.append(self.senseHat.getPitch())
+                        self.currRoll.append(self.senseHat.getRoll())
+                        self.currYaw.pop(0)
+                        self.currPitch.pop(0)
+                        self.currRoll.pop(0)
+            except:
+                pass
+            finally:
+                self.lock.release()
 
-                    # get orientation()
-
-                    self.currYaw.append(newYaw)
-                    self.currPitch.append(newPitch)
-                    self.currRoll.append(newRoll)
-                    self.currYaw.pop(0)
-                    self.currPitch.pop(0)
-                    self.currRoll.pop(0)
-
-                # After all, release lock
-                self.condition.notifyAll()
+            sleep((1 / 1000) * THREAD_SLEEP_MS)
 
         return
--- modul/i2cModules/senseHatAdapter.py
+++ modul/i2cModules/senseHatAdapter.py
@@ -13,14 +13,13 @@
 from sense_hat import SenseHat
 
 # Class
-class SenseHatAdapter:
+class SenseHatAdapter():
     # Konstruktor
     # --------------------------------------------------------------------------
     def __init__(self):
         self.orientationPitch = 0.0
         self.orientationRoll = 0.0
         self.orientationYaw = 0.0
-        self.orientationNorth = 0.0
         self.displayBuffer = [[0, 0, 0]] * 64
         self.ForeColor = [255,255,255]          # White
         self.ForeColorIdle = [255, 127, 39]     # Orange
@@ -38,56 +37,23 @@ class SenseHatAdapter:
         self.sense.set_imu_config(True, True, True)
         return
 
-    def setStatusLed(self, newNumber, newStatus):
-        """
-        Description: Sets the status for a given module/whatever and shows it
-                     at the bottom of the RGB-Display.
-                     Eight states from 0-7 are adjustable.
-                     Possible states are:
-                     "IDLE", "RUNNING", "STOPPED" -> Orange, Green, Red
-        Args:        1. ID as integer (0-7)
-                     2. Status as integer (0=IDLE, 1=RUNNING, 2=STOPPED)
-        """
-        if(0 <= newNumber and newNumber <= 7):
-            pixelPosition = [56, 57, 58, 59, 60, 61, 62, 63]
-            currentColor = self.ForeColorIdle
-
-            if(newStatus == 1):
-                currentColor = self.ForeColorRunning
-            elif(newStatus == 2):
-                currentColor = self.ForeColorStop
-
-            self.displayBuffer[pixelPosition[newNumber]] = currentColor
-
-        return
-
-    def setRomanNumber(self, newNumber):
-        """
-        Description: Shows a roman number ont the RGB-Display.
-                     Possible Numbers are: "I, II, III, IV, V".
-                     Clears automatically the area for the number.
-        Args:        1. Number as integer or string
-        """
-        self.drawChar('!', [0, 0, 0])
-        self.drawChar(str(newNumber), self.ForeColor)
-
-        return
 
     def refreshOrientation(self):
         """
         Description: Reads the IMU-values (pitch/roll/yaw) of the Sense-Hat
         """
-        currGyroData = self.sense.get_orientation()
-        currCompassData = self.sense.get_compass()
+        try:
+            currGyroData = self.sense.get_orientation()
 
-        if currGyroData is not None:
-            self.orientationPitch = currGyroData.get('pitch')
-            self.orientationRoll = currGyroData.get('roll')
-            self.orientationYaw = currGyroData.get('yaw')
+            if currGyroData is not None:
+                self.orientationPitch = currGyroData.get('pitch')
+                self.orientationRoll = currGyroData.get('roll')
+                self.orientationYaw = currGyroData.get('yaw')
 
-        if currCompassData is not None:
-            self.orientationNorth = currCompassData
-        return
+            return True
+        except:
+            return False
+
 
     def getPitch(self):
         """
@@ -96,12 +62,14 @@ class SenseHatAdapter:
         """
         return self.orientationPitch
 
+
     def getRoll(self):
         """
         Description: Gets the roll-value from the last forced measurement
         Returns:     Float
         """
         return self.orientationRoll
+
 
     def getYaw(self):
         """
@@ -110,12 +78,6 @@ class SenseHatAdapter:
         """
         return self.orientationYaw
 
-    def getNorth(self):
-        """
-        Description: Gets the north-value from the last forced measurement
-        Returns:     Float
-        """
-        return self.orientationNorth
 
     def drawChar(self, newChar, newColor):
         """
@@ -135,6 +97,7 @@ class SenseHatAdapter:
 
         return
 
+
     def clearDisplay(self):
         """
         Description: Clears the whole RGB-Display
@@ -151,17 +114,69 @@ class SenseHatAdapter:
 
         return
 
+
+    def setStatusLeds(self, listOfStates):
+        """
+        Description: Gets a whole list of states and calls setStatusLed for
+                     every item.
+        Args:        1. list with states
+        """
+        if(listOfStates is not None):
+            count = len(listOfStates)
+
+            for x in range(count):
+                self.setStatusLed(x, listOfStates[x])
+        return
+
+
+    def setStatusLed(self, newNumber, newState):
+        """
+        Description: Sets the status for a given module/whatever and shows it
+                     at the bottom of the RGB-Display.
+                     Eight states from 0-7 are adjustable.
+                     Possible states are:
+                     "IDLE", "RUNNING", "STOPPED" -> Orange, Green, Red
+        Args:        1. ID as integer (0-7)
+                     2. Status as integer (0=IDLE, 1=RUNNING, 2=STOPPED)
+        """
+        if(0 <= newNumber and newNumber <= 7):
+            pixelPosition = [56, 57, 58, 59, 60, 61, 62, 63]
+            currentColor = self.ForeColorIdle
+
+            if(newState == 1):
+                currentColor = self.ForeColorRunning
+            elif(newState == 2):
+                currentColor = self.ForeColorStop
+
+            self.displayBuffer[pixelPosition[newNumber]] = currentColor
+
+        return
+
+
+    def setRomanNumber(self, newNumber):
+        """
+        Description: Shows a roman number ont the RGB-Display.
+                     Possible Numbers are: "I, II, III, IV, V".
+                     Clears automatically the area for the number.
+        Args:        1. Number as integer or string
+        """
+        self.drawChar("clear", [0, 0, 0])
+        self.drawChar(str(newNumber), self.ForeColor)
+
+        return
+
+
     def getCharCode(self, newChar):
         """
         Description: Draw-Matrix for the numbers
         Returns:     List[]
         """
         return {
-            '0': [10, 11, 12, 13, 18, 21, 26, 29, 34, 37, 42, 43, 44, 45],
-            '1': [13, 21, 29, 37, 45],
-            '2': [10, 11, 12, 13, 21, 26, 27, 28, 29, 34, 42, 43, 44, 45],
-            '3': [10, 11, 12, 13, 21, 27, 28, 29, 37, 42, 43, 44, 45],
-            '4': [10, 13, 18, 21, 26, 27, 28, 29, 37, 45],
-            '5': [10, 11, 12, 13, 18, 26, 27, 28, 29, 37, 42, 43, 44, 45],
-            '!': [10, 11, 12, 13, 18, 21, 26, 27, 28, 29, 34, 37, 42, 43, 44, 45]
+            "0": [10, 11, 12, 13, 18, 21, 26, 29, 34, 37, 42, 43, 44, 45],
+            "1": [13, 21, 29, 37, 45],
+            "2": [10, 11, 12, 13, 21, 26, 27, 28, 29, 34, 42, 43, 44, 45],
+            "3": [10, 11, 12, 13, 21, 27, 28, 29, 37, 42, 43, 44, 45],
+            "4": [10, 13, 18, 21, 26, 27, 28, 29, 37, 45],
+            "5": [10, 11, 12, 13, 18, 26, 27, 28, 29, 37, 42, 43, 44, 45],
+            "clear": [10, 11, 12, 13, 18, 21, 26, 27, 28, 29, 34, 37, 42, 43, 44, 45]
         }.get(newChar, [0])  # 0 is default
