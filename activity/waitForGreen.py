03d31e96c802c4bb9e0071babad9cff664881d91
disperate <julian.bigler@hotmail.com>
2017-04-05 10:32:29 +0200

    added config file with greenlight timeout

--- activity/waitForGreen.py
+++ activity/waitForGreen.py
@@ -1,9 +1,22 @@
 import time
+import config
 
 class waitForGreenActivity(object):
+
+
     def __init__(self, fsm, cameraModul):
+        self.startTime = time.time()
         print("waiting for green...")
         cameraModul.startGreenlightDedection()
-        time.sleep(3)
+
+
+        while(not cameraModul.isGreen() and not self.timeout()):
+            time.sleep(0.1)
+
         print("green")
         fsm.greenlight()
+
+    def timeout(self):
+        secondsToTimeout = config.greenlightTimeoutInSeconds
+        return time.time() - self.startTime > secondsToTimeout
+
---
+++ config.py
@@ -0,0 +1,1 @@
+greenlightTimeoutInSeconds = 20
