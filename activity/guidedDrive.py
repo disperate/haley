35e7e63e39cdc644a1bee153581bd7402c31131d
disperate <julian.bigler@hotmail.com>
2017-04-21 11:16:27 +0200

    added i2c to statemachine

--- activity/guidedDrive.py
+++ activity/guidedDrive.py
@@ -5,10 +5,11 @@ import config
 
 class guidedDriveActivity(Thread):
 
-    def __init__(self, fsm, motor):
+    def __init__(self, fsm, motor, i2c):
         super().__init__()
         self._running = True
         self._motorController = motor
+        self._i2c = i2c
 
     def terminate(self):
         self._motorController.setVelocityLeft(0.0)
@@ -17,6 +18,8 @@ class guidedDriveActivity(Thread):
 
     def run(self):
         while(self._running):
+            error = self._i2c.getDistanceLeftBack() - self._i2c.getDistanceRightBack()
+            print(error)
             self._motorController.setVelocityLeft(config.guidedDriveVelocity)
             self._motorController.setVelocityRight(config.guidedDriveVelocity)
             time.sleep(0.1)
