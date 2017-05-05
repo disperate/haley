4ec76577e8a7801c35191d001160e1b6f34f8850
ustoeckli <urs.stoeckli@stud.hslu.ch>
2017-05-05 23:10:15 +0200

    PID_2 für Test

--- test_PID_2.py
+++ test_PID_2.py
@@ -15,12 +15,12 @@ try:
     i2c = i2cHandler.I2cHandler()
     i2c.start()
 
-    pid_dist = pid.PID(0.01, 0, 0)
+    pid_dist = pid.PID(0.05, 0, 0)
     pid_dist.setWindup(0.5)
     pid_dist.sample_time = 0.1
     soll_angle = 0
 
-    pid_angle = pid.PID(0.1, 0, 0)
+    pid_angle = pid.PID(0.02, 0, 0)
     pid_angle.setWindup(0.5)
     pid_angle.sample_time = 0.1
 
@@ -31,26 +31,40 @@ try:
     regD_out_str = ""
     regW_out_str = ""
 
+    cnt = 1
+    while(True):
+        print("\033c")
+        print("Wait for Distance-Data (Try " + str(cnt) + "): " + \
+             str(i2c.getDistanceLeftFront()) +", "+ str(i2c.getDistanceRightFront()) +", "+\
+             str(i2c.getDistanceLeftBack()) +", "+ str(i2c.getDistanceLeftBack()))
+        if (i2c.getDistanceLeftFront() > 0):
+            if (i2c.getDistanceLeftBack() > 0):
+                if (i2c.getDistanceRightFront() > 0):
+                    if (i2c.getDistanceRightBack() > 0):
+                        break;
+        cnt = cnt + 1
+        sleep(0.1)
+
     while (True):
         ist_dist = i2c.getDistanceLeftBack() - i2c.getDistanceRightBack()
         ist_angle = atan((i2c.getDistanceLeftFront() - (i2c.getDistanceLeftBack()-0.5)) / 180)
         pid_dist.SetPoint = 0.0
-        #if pid_dist.update(ist_dist):
-        #    soll_angle = pid_dist.output
-        #    pid_angle.SetPoint = soll_angle
-        #    regD_out_str = str(round(pid_dist.SetPoint, 3)) +"; " + str(round(ist_dist, 3)) + "; " +\
-        #                   str(round(pid_dist.SetPoint-ist_dist, 3)) +"; "+ str(round(soll_angle, 1)) +"; ;"
+        if pid_dist.update(ist_dist):
+            soll_angle = pid_dist.output
+            pid_angle.SetPoint = soll_angle
+            regD_out_str = str(round(pid_dist.SetPoint, 3)) +"; " + str(round(ist_dist, 3)) + "; " +\
+                           str(round(pid_dist.SetPoint-ist_dist, 3)) +"; "+ str(round(soll_angle, 1)) +"; ;"
 
-        if pid_angle.update(ist_angle):
-            DeltaVelocityLeft_proz = pid_angle.output
-            VelocityLeft_proz = 1 + DeltaVelocityLeft_proz
-        #        motor.setVelocityLeft(config.guidedDriveVelocity * VelocityLeft_proz)
-        #        motor.setVelocityRight(config.guidedDriveVelocity)
-            motor.setVelocityLeft(5000 * DeltaVelocityLeft_proz)
-            motor.setVelocityRight(5000 * -DeltaVelocityLeft_proz)
-            regW_out_str = str(round(soll_angle, 3)) +" ; "+ str(round(ist_angle, 3)) +" ; "+\
-                           str(round(soll_angle-ist_angle, 3)) +" ; "+\
-                           str(round(DeltaVelocityLeft_proz,5))
+            if pid_angle.update(ist_angle):
+                DeltaVelocityLeft_proz = pid_angle.output
+                VelocityLeft_proz = 1 + DeltaVelocityLeft_proz
+                motor.setVelocityLeft(config.guidedDriveVelocity)
+                motor.setVelocityRight(config.guidedDriveVelocity * VelocityLeft_proz)
+                #motor.setVelocityLeft(5000 * -DeltaVelocityLeft_proz)
+                #motor.setVelocityRight(5000 * DeltaVelocityLeft_proz)
+                regW_out_str = str(round(soll_angle, 3)) +" ; "+ str(round(ist_angle, 3)) +" ; "+\
+                               str(round(soll_angle-ist_angle, 3)) +" ; "+\
+                               str(round(DeltaVelocityLeft_proz,5))
 
             #print("\033c")
             print(str(round(pid_dist.current_time,3)) +"; "+ regD_out_str + regW_out_str)
