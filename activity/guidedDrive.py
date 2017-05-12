7026cf97e054be1f2c2a0159ed8fdb39e37d41a0
ustoeckli <urs.stoeckli@stud.hslu.ch>
2017-05-12 10:36:53 +0200

    PID

--- activity/guidedDrive.py
+++ activity/guidedDrive.py
@@ -13,14 +13,14 @@ class guidedDriveActivity(Thread):
         self._motorController = motor
         self._i2c = i2c
 
-        pid_dist = pid.PID(0.002, 0, 0)
-        pid_dist.setWindup(0.5)
-        pid_dist.sample_time = 0.1
-        soll_angle = 0
+        self.pid_dist = pid.PID(0.002, 0, 0)
+        self.pid_dist.setWindup(0.5)
+        self.pid_dist.sample_time = 0.1
+        self.soll_angle = 0
 
-        pid_angle = pid.PID(1.5, 0, 0)
-        pid_angle.setWindup(0.5)
-        pid_angle.sample_time = 0.1
+        self.pid_angle = pid.PID(1.5, 0, 0)
+        self.pid_angle.setWindup(0.5)
+        self.pid_angle.sample_time = 0.1
 
     def terminate(self):
         self._running = False
@@ -29,14 +29,14 @@ class guidedDriveActivity(Thread):
         while(self._running):
             ist_dist = self._i2c.getDistanceLeftBack() - self._i2c.getDistanceRightBack()
             ist_angle = atan((self._i2c.getDistanceLeftFront() - (self._i2c.getDistanceLeftBack() - 0.5)) / 180)
-            pid_dist.SetPoint = 0.0
-            if pid_dist.update(ist_dist):
-                soll_angle = pid_dist.output
-                pid_angle.SetPoint = soll_angle
+            self.pid_dist.SetPoint = 0.0
+            if self.pid_dist.update(ist_dist):
+                self.soll_angle = self.pid_dist.output
+                self.pid_angle.SetPoint = self.soll_angle
 
-            if pid_angle.update(ist_angle):
-                DeltaVelocityLeft_proz = pid_angle.output
+            if self.pid_angle.update(ist_angle):
+                DeltaVelocityLeft_proz = self.pid_angle.output
                 VelocityLeft_proz = 1 + DeltaVelocityLeft_proz
                 _motorController.setVelocityLeft(config.guidedDriveVelocity)
-
+                _motorController.setVelocityRight(config.guidedDriveVelocity * VelocityLeft_proz)
             sleep(0.02)
