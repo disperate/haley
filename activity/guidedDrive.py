176cc6e0123335c7178860c7d5337dbf081746e0
ustoeckli <urs.stoeckli@stud.hslu.ch>
2017-05-19 10:22:59 +0200

    Modul Regler with mode TO_MIDDLE, TO_LEFT, TO_RIGHT

--- activity/guidedDrive.py
+++ activity/guidedDrive.py
@@ -3,6 +3,8 @@ from math import atan
 from time import sleep
 import config
 from common import pid
+from common import regler
+from haleyenum.reglerMode import reglerMode
 
 
 class guidedDriveActivity(Thread):
@@ -12,38 +14,41 @@ class guidedDriveActivity(Thread):
         self._running = True
         self._motorController = motor
         self._i2c = i2c
+        self._regler(_motorController, _i2c, reglerMode.TO_MIDDLE)
 
-        self.pid_dist = pid.PID(0.002, 0, 0)
-        self.pid_dist.setWindup(0.5)
-        self.pid_dist.sample_time = 0.1
-        self.soll_angle = 0
+        #self.pid_dist = pid.PID(0.002, 0, 0)
+        #self.pid_dist.setWindup(0.5)
+        #self.pid_dist.sample_time = 0.1
+        #self.soll_angle = 0
 
-        self.pid_angle = pid.PID(1.5, 0, 0)
-        self.pid_angle.setWindup(0.5)
-        self.pid_angle.sample_time = 0.1
+        #self.pid_angle = pid.PID(1.5, 0, 0)
+        #self.pid_angle.setWindup(0.5)
+        #self.pid_angle.sample_time = 0.1
 
     def terminate(self):
         self._running = False
 
     def run(self):
         while(self._running):
-            if ((self._i2c.getDistanceLeftBack() > config.loseWallsDistanceThreshold) or
-                (self._i2c.getDistanceLeftFront() > config.loseWallsDistanceThreshold) or
-                (self._i2c.getDistanceRightBack() > config.loseWallsDistanceThreshold) or
-                (self._i2c.getDistanceRightFront() > config.loseWallsDistanceThreshold)    ):
-                self._motorController.setVelocityLeft(config.guidedDriveVelocity)
-                self._motorController.setVelocityRight(config.guidedDriveVelocity)
-            else:
-                ist_dist = self._i2c.getDistanceLeftBack() - self._i2c.getDistanceRightBack()
-                ist_angle = atan((self._i2c.getDistanceLeftFront() - (self._i2c.getDistanceLeftBack() - 0.5)) / 180)
-                self.pid_dist.SetPoint = 0.0
-                if self.pid_dist.update(ist_dist):
-                    self.soll_angle = self.pid_dist.output
-                    self.pid_angle.SetPoint = self.soll_angle
+            self._regler.update()
 
-                if self.pid_angle.update(ist_angle):
-                    DeltaVelocityLeft_proz = self.pid_angle.output
-                    VelocityLeft_proz = 1 + DeltaVelocityLeft_proz
-                    self._motorController.setVelocityLeft(config.guidedDriveVelocity)
-                    self._motorController.setVelocityRight(config.guidedDriveVelocity * VelocityLeft_proz)
+            #if ((self._i2c.getDistanceLeftBack() > config.loseWallsDistanceThreshold) or
+            #    (self._i2c.getDistanceLeftFront() > config.loseWallsDistanceThreshold) or
+            #    (self._i2c.getDistanceRightBack() > config.loseWallsDistanceThreshold) or
+            #    (self._i2c.getDistanceRightFront() > config.loseWallsDistanceThreshold)    ):
+            #    self._motorController.setVelocityLeft(config.guidedDriveVelocity)
+            #    self._motorController.setVelocityRight(config.guidedDriveVelocity)
+            #else:
+            #    ist_dist = self._i2c.getDistanceLeftBack() - self._i2c.getDistanceRightBack()
+            #    ist_angle = atan((self._i2c.getDistanceLeftFront() - (self._i2c.getDistanceLeftBack() - 0.5)) / 180)
+            #    self.pid_dist.SetPoint = 0.0
+            #    if self.pid_dist.update(ist_dist):
+            #        self.soll_angle = self.pid_dist.output
+            #        self.pid_angle.SetPoint = self.soll_angle
+
+            #    if self.pid_angle.update(ist_angle):
+            #        DeltaVelocityLeft_proz = self.pid_angle.output
+            #        VelocityLeft_proz = 1 + DeltaVelocityLeft_proz
+            #        self._motorController.setVelocityLeft(config.guidedDriveVelocity)
+            #        self._motorController.setVelocityRight(config.guidedDriveVelocity * VelocityLeft_proz)
             sleep(0.02)
