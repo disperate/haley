502c854a60aaff4e6549f26f8df7ccdaa1f3b2dd
disperate <julian.bigler@hotmail.com>
2017-07-01 12:57:02 +0200

    exculded greenlight logic from camera modul

--- modul/camera.py
+++ modul/camera.py
@@ -1,13 +1,10 @@
-from threading import Thread
-import picamera
-import numpy as np
-import cv2
 import time
+from threading import Thread
 
-redValue = 175
-greenValue = 60
-sensitivity = 15
-font = cv2.FONT_HERSHEY_SIMPLEX
+import numpy as np
+import picamera
+
+import modul.greenLightDedection
 
 
 class camera(Thread):
@@ -21,6 +18,7 @@ class camera(Thread):
         self._camera = picamera.PiCamera()
         self._camera.resolution = (640, 480)
         self._camera.framerate = 24
+        self._greenLightDedection = modul.greenLightDedection.greenLightDedection()
 
     def startGreenlightDedection(self):
         self._dedectGreenLight = True
@@ -52,48 +50,10 @@ class camera(Thread):
             image = image.reshape((480, 640, 3))
 
             if self._dedectGreenLight:
-                print("looking for green light...")
-                self._dedectAmpel(image)
+                self.isGreen = self._greenLightDedection.dedectAmpel(image)
+                if self.isGreen:
+                    self.stopGreenlightDedection()
 
             if self._dedectRomanNumber:
                 print("looking for roman nummber")
             time.sleep(0.01)
-
-    def _green(self, img):
-        lower_red = np.array([greenValue - sensitivity, 100, 50])
-        upper_red = np.array([greenValue + sensitivity, 255, 255])
-        mask = cv2.inRange(img, lower_red, upper_red)
-        onlygreenframe = cv2.bitwise_and(img, img, mask=mask)
-
-        return self._dedectCircles(onlygreenframe)
-
-    def _red(self, img):
-        lower_red = np.array([redValue - sensitivity, 100, 100])
-        upper_red = np.array([redValue + sensitivity, 255, 255])
-        mask = cv2.inRange(img, lower_red, upper_red)
-        onlyredframe = cv2.bitwise_and(img, img, mask=mask)
-
-        kernel = np.ones((3, 3), np.uint8)
-
-        img_erosion = cv2.erode(onlyredframe, kernel, iterations=1)
-
-        return self._dedectCircles(img_erosion)
-
-    def _toHSV(self, img):
-        return cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
-    def _dedectCircles(self, img):
-        res = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        circles = cv2.HoughCircles(res, cv2.HOUGH_GRADIENT, 1, 20, param1=10, param2=10, minRadius=3, maxRadius=20)
-        if circles is not None:
-            return True
-        return False
-
-    def _dedectAmpel(self, img):
-        hsv = self._toHSV(img)
-        if self._red(hsv):
-            print("red.")
-        if self._green(hsv):
-            self.isGreen = True
-            print("Let's go. It's Green. GREEN!")
-            self.stopGreenlightDedection()
